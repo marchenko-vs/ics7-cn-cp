7cc073155080301d9fa34bee1e504339f4fd5adc
Vladyslav Marchenko <marchenko.vlse@gmail.com>
2023-11-28 04:13:39 -0800

    Almost ready

--- src/server.c
+++ src/server.c
@@ -59,11 +59,11 @@ size_t parse_extension(const char *const filename, char *buffer)
 void send_data(const char *const filename, const int fd, const struct sockaddr *to,
 	             socklen_t tolen)
 {
-	char buffer[64 + 1] = "";
+	char buffer[512 + 1] = "";
 	FILE *f = fopen(filename, "rb");
 	ssize_t rlen = 0, wlen = 0;
 
-	while ((wlen != -1) && ((rlen = fread(buffer, sizeof(char), 64, f)) > 0))
+	while ((wlen != -1) && ((rlen = fread(buffer, sizeof(char), 512, f)) > 0))
 	{
 		wlen = sendto(fd, buffer, rlen, 0, to, tolen);
 	}
